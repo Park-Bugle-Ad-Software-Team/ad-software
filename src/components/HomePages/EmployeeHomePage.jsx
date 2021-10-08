eaea66eff4e21858750abdd40b35506ca034a823
Edan Schwartz <edanschwartz@gmail.com>
2021-10-08 10:24:53 -0500

    Revert changes to EmployeeHomePage
    caused by wonky feature/registration merge conflicts

--- src/components/HomePages/EmployeeHomePage.jsx
+++ src/components/HomePages/EmployeeHomePage.jsx
@@ -1,28 +1,34 @@
-import { Typography, Button, FormControl, InputLabel, Select, MenuItem, Grid } from '@mui/material';
+import { Typography, Button, FormControl, InputLabel, Select, MenuItem, Grid, Input } from '@mui/material';
+import { useDispatch, useSelector } from 'react-redux';
+import React, { useEffect, useState } from 'react';
+import PendingContractsEmployeeView from '../Contracts/PendingContractsEmployeeView';
+import AllContracts from '../Contracts/AllContracts';
 
 export default function EmployeeHomePage() {
-    // test data
-    let sonia = {
-        firstName: 'Sonia',
-        lastName: 'Becerra'
-    };
+    const dispatch = useDispatch();
 
-    let advertisers = [
-        {
-            name: 'Chroma Zone'
-        },
-        {
-            name: 'Ace Hardware'
-        },
-        {
-            name: 'Park Bugle'
-        }
-    ];
+    useEffect(() => {
+        dispatch({type: 'FETCH_ADVERTISERS'});
+    }, []);
 
-    let months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+    // local state
+    let [filterContract, setFilterContract] = useState({
+        advertiser: '', startMonth: '', endMonth: '', contractStatus: ''
+    });
 
-    let years = [2020, 2021, 2022, 2023];
+    // global state from redux
+    const store = useSelector((store) => store)
+    const user = store.user;
+    const advertisers = store.advertisers;
+    const pendingContracts = store.pendingContracts;
+    const allContracts = store.allContracts;
 
+    // constants for search drop down criteria
+    const statuses = ['Active', 'Closed'];
+
+    function fetchFilteredContracts() {
+        dispatch({type: 'FETCH_FILTERED_CONTRACTS'});
+    }
 
     return (
         <>
@@ -31,20 +37,20 @@ export default function EmployeeHomePage() {
                     <Typography variant="h1">Employee Home Page</Typography>  
                 </Grid>
                 <Grid item xs={8}>
-                    User: {sonia.firstName} {sonia.lastName}
+                    User: {user.name}
                 </Grid>
                 <Grid item xs={4}>
                     <Button 
                         variant="contained" 
                         color="primary"
-                        // onClick
+                        onClick={() => console.log('in Export button')}
                     >
                         Export
                     </Button>
                     <Button 
                         variant="contained"
                         color="primary"
-                        // onClick
+                        onClick={() => console.log('in Create New Ad Contract button')}
                     >
                         Create New Ad Contract
                     </Button>
@@ -56,46 +62,117 @@ export default function EmployeeHomePage() {
                             labelId="advertiser-select-label"
                             id="advertiser-select"
                             label="Advertiser"
-                            // onChange
+                            value={filterContract.advertiser}
+                            onChange={(event) => setFilterContract({...filterContract, advertiser: event.target.value})}
                         >
-                            {advertisers.map(advertiser => (
-                                <MenuItem value={advertiser.name}>{advertiser.name}</MenuItem>
+                            {advertisers.map((advertiser, i) => (
+                                <MenuItem key={i} value={advertiser.companyName}>
+                                    {advertiser.companyName}
+                                </MenuItem>
                             ))}
                         </Select>
                     </FormControl>
                     <FormControl>
-                        <InputLabel id="month-select-label">Month</InputLabel>
-                        <Select
-                            labelId="month-select-label"
-                            id="month-select"
-                            label="Month"
-                            // onChange
-                        >
-                            {months.map(month => (
-                                <MenuItem value={month}>{month}</MenuItem>
-                            ))}
-                        </Select>
+                        <InputLabel id="start-month-select-label">Start Month</InputLabel>
+                        <Input
+                            id="start-month-select"
+                            label="Start Month"
+                            value={filterContract.startMonth}
+                            type="month"
+                            onChange={(event) => setFilterContract({...filterContract, startMonth: event.target.value})}
+                        />
                     </FormControl>
                     <FormControl>
-                        <InputLabel id="year-select-label">Year</InputLabel>
+                        <InputLabel id="end-month-select-label">End Month</InputLabel>
+                        <Input
+                            id="end-month-select"
+                            label="End Month"
+                            value={filterContract.endMonth}
+                            type="month"
+                            onChange={(event) => setFilterContract({...filterContract, endMonth: event.target.value})}
+                        />
+                    </FormControl>
+                    <FormControl>
+                        <InputLabel id="contract-status-select-label">Status</InputLabel>
                         <Select
-                            labelId="year-select-label"
-                            id="year-select"
-                            label="Year"
-                            // onChange
+                            labelId="contract-status-select-label"
+                            id="contract-status-select"
+                            label="Status"
+                            value={filterContract.contractStatus}
+                            onChange={(event) => setFilterContract({...filterContract, contractStatus: event.target.value})}
                         >
-                            {years.map(year => (
-                                <MenuItem value={year}>{year}</MenuItem>
+                            {statuses.map((status, i) => (
+                                <MenuItem key={i} value={status}>{status}</MenuItem>
                             ))}
                         </Select>
                     </FormControl>
+                    <Button 
+                        variant="contained" 
+                        color="primary"
+                        onClick={fetchFilteredContracts}
+                    >
+                        Search
+                    </Button>
+                </Grid>
+
+                {/* Pending Contracts component */}
+                <Grid item xs={12}>
+                    <center>
+                        <Typography variant="h3">Pending Contracts</Typography>  
+                    </center>
                 </Grid>
                 <Grid item xs={12}>
-                    {/* component for listing out pending contracts */}
+                    <table className="uTable">
+                        <thead>
+                            <tr className="uTr">
+                                <th className="uTh">Start Month</th>
+                                <th className="uTh">Contract Length</th>
+                                <th className="uTh">Type</th>
+                                <th className="uTh">Size</th>
+                                <th className="uTh">Page</th>
+                                <th className="uTh">Color</th>
+                                <th className="uTh">Total Cost</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {pendingContracts.map((item, i) => (
+                                <tr className="uTr" key={i}>
+                                    <PendingContractsEmployeeView item={item} />
+                                </tr>
+                            ))}
+                        </tbody>
+                    </table> 
+                </Grid>
+
+                {/* All Contracts component */}
+                <Grid item xs={12}>
+                    <center>
+                        <Typography variant="h3">All Contracts</Typography>  
+                    </center>
                 </Grid>
                 <Grid item xs={12}>
-                    {/* component for listing out approved contracts */}
+                    <table className="uTable">
+                        <thead>
+                            <tr className="uTr">
+                                <th className="uTh">Start Month</th>
+                                <th className="uTh">Contract Length</th>
+                                <th className="uTh">Type</th>
+                                <th className="uTh">Size</th>
+                                <th className="uTh">Page</th>
+                                <th className="uTh">Color</th>
+                                <th className="uTh">Total Cost</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {allContracts.map((item, i) => (
+                                <tr className="uTr" key={i}>
+                                    <AllContracts item={item} />
+                                </tr>
+                            ))}
+                        </tbody>
+                    </table> 
                 </Grid>
+
             </Grid>
         </>
     )
