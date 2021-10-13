b9c507d85170e3e165e0828e893283d718b67f26
Eben Stine <ebenstine@gmail.com>
2021-10-13 10:45:21 -0500

    styled progress bar

--- src/components/ImageUploader/ProgressBar.jsx
+++ src/components/ImageUploader/ProgressBar.jsx
@@ -1,16 +1,29 @@
 import React from 'react';
-import LinearProgress from '@mui/material/LinearProgress';
+import { LinearProgress } from '@mui/material';
 import Typography from "@mui/material/Typography";
 // import { black } from '@material-ui/core/colors';
+import { createTheme } from '@mui/material/styles';
+import { ThemeProvider } from '@mui/material/styles';
 
-
+const theme = createTheme({
+    palette: {
+        primary: {
+            //dark grey
+            main: "#3D3D3D",
+        },
+          secondary: {
+            //lighter grey
+            main: '#858585',
+          },
+    },
+});
 
 const ProgressBar = ({ progress, progressTitle }) => {
     //const { root } = useStyles();
 
     return (
         <>
-            
+            <ThemeProvider theme={theme}>
                 {progress > 0 &&
                     <>
                         <Typography>
@@ -21,7 +34,7 @@ const ProgressBar = ({ progress, progressTitle }) => {
                         </div>
                     </>
                 }
-            
+            </ThemeProvider>
         </>
     )
 }
