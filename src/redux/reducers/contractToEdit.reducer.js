7edd12cd14d9b9f091cba2bf0ef92fa84d7d0484
Garrett J Harty <gharty@live.com>
2021-10-18 09:07:23 -0500

    updated base state for presentation

--- src/redux/reducers/contractToEdit.reducer.js
+++ src/redux/reducers/contractToEdit.reducer.js
@@ -1,29 +1,29 @@
-// let baseState = {
-//   actualBill: 0.00,
-//   actualColumns: 0,
-//   actualInches: 0,
-//   adRepId: 11,
-//   adRepName: '',
-//   adSizeId: 7,
-//   advertiserId: 2,
-//   colorId: 1,
-//   commissionPercentage: 20,
-//   companyName: '',
-//   contractType: 'Print',
-//   designerId: 10,
-//   designerName: '',
-//   image: [],
-//   months: 1,
-//   notes: '',
-//   page: 5
-// };
+let baseState = {
+  actualBill: 200.00,
+  actualColumns: 2,
+  actualInches: 4,
+  adRepId: 3,
+  adRepName: 'Sonia Becerra',
+  adSizeId: 6,
+  advertiserId: 2,
+  colorId: 1,
+  commissionPercentage: 20,
+  companyName: '',
+  contractType: 'Print',
+  designerId: 10,
+  designerName: 'Kelly Brooks',
+  image: [],
+  months: 1,
+  notes: '',
+  page: 5
+};
 
-const contractToEditReducer = (state = {}, action) => {
+const contractToEditReducer = (state = baseState, action) => {
     switch (action.type) {
       case 'SET_CONTRACT_TO_EDIT':
         return action.payload;
       case 'UNSET_CONTRACT_TO_EDIT':
-        return {};
+        return baseState;
       case 'UPDATE_CONTRACT_TO_EDIT':
         return action.payload;
       case 'INSERT_AD_SIZE':
