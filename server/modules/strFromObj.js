e25a73922368ee60df5c09945147b83c78546b5a
Dan St. Aubin <75379416+staubind@users.noreply.github.com>
2021-10-20 16:07:26 -0500

    Update strFromObj.js

--- server/modules/strFromObj.js
+++ server/modules/strFromObj.js
@@ -1,3 +1,4 @@
+// creates a string from an object, used for writing dynamic sql queries
 function strFromObj(obj, separator, callback) {
     return Object.keys(obj).map(callback).join(separator)
 }
