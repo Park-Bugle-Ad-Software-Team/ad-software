e98e8a78efcdeb4b366431cb9f460e36383e474c
Edan Schwartz <edanschwartz@gmail.com>
2021-11-11 13:53:50 -0600

    server code cleanup

--- server/server.js
+++ server/server.js
@@ -18,7 +18,6 @@ const chatRouter = require('./routes/chat.router');
 const contractsRouter = require('./routes/contracts.router');
 const ratesRouter = require('./routes/rates.router');
 const adSizeRouter = require('./routes/adSize.router')
-const s3Uploader = require('react-s3-uploader/s3router');
 
 // Body parser middleware
 app.use(bodyParser.json());
@@ -37,21 +36,6 @@ app.use('/api/chat', chatRouter);
 app.use('/api/contracts', contractsRouter);
 app.use('/api/rates', ratesRouter);
 app.use('/api/ad-size', adSizeRouter)
-/* app.use('/s3', s3Uploader({
-  bucket: process.env.REACT_APP_AWS_S3_BUCKET, // required
-  region: process.env.REACT_APP_AWS_REGION,      // optional
-  headers: {'Access-Control-Allow-Origin': '*'}, // optional
-  ACL: 'private',   
-  uniquePrefix: true        // this is the default - set to `public-read` to let anyone view uploads
-}));
-
-console.log('uploader', {
-  bucket: process.env.REACT_APP_AWS_S3_BUCKET, // required
-  region: process.env.REACT_APP_AWS_REGION,      // optional
-  headers: {'Access-Control-Allow-Origin': '*'}, // optional
-  ACL: 'private',   
-  uniquePrefix: true        // this is the default - set to `public-read` to let anyone view uploads
-}) */
 
 // Serve static files
 app.use(express.static('build'));
@@ -61,17 +45,13 @@ app.use(express.static('build'));
 const s3 = new aws.S3();
 app.post('/api/file', upload.single('file'), async(req, res) => {
   try {
-    console.log('uploading', {
-      Bucket: process.env.AWS_S3_BUCKET,
-      Key: `${Date.now()}-${req.file.filename}`,
-    })
     const s3Res = await s3.upload({
       Bucket: process.env.AWS_S3_BUCKET,
       Key: `${Date.now()}-${req.file.originalname}`,
       Body: req.file.buffer,
       ACL: 'public-read',
     }).promise();
-    console.log('upload complete')
+
     res.send({
       location: s3Res.Location
     });
